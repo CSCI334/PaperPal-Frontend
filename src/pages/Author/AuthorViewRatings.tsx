fbd344b3007ea497fe8ba4b4cd3ef37fbe533264
Aric-prog <hernandoaric@gmail.com>
2023-04-21 01:26:12 +1000

    fixed lorem import (it's case sensitive)

--- src/pages/Author/AuthorViewRatings.tsx
+++ src/pages/Author/AuthorViewRatings.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import PDFView from "../../components/PDFView/PDFView";
 import { Box } from "@mui/system";
-import Lorem_Ipsum from "../../components/PDFView/Lorem_Ipsum.pdf";
+import Lorem_Ipsum from "../../components/PDFView/Lorem_ipsum.pdf";
 
 //This class Renders the AuthorView ratings page and deals with all components necessary for render
 const AuthorViewRatings: React.FC = () => {
-    const pdfFile = Lorem_Ipsum;
+  const pdfFile = Lorem_Ipsum;
 
-    return (
-        <Box>
-            <PDFView key={pdfFile}
-                 file={pdfFile}
-            />
-        </Box>
-    );
+  return (
+    <Box>
+      <PDFView key={pdfFile} file={pdfFile} />
+    </Box>
+  );
 };
 export default AuthorViewRatings;
