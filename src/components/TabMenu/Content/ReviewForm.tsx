ea5da2021147915c157a2fbd02e8a4cc44399f8e
trutina <ericedgari@hotmail.com>
2023-05-25 16:05:35 +1000

    review form for author have current rating

--- src/components/TabMenu/Content/ReviewForm.tsx
+++ src/components/TabMenu/Content/ReviewForm.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import getReviews from "../../../services/getReviews";
 import getUser from "../../../services/account/getUser";
 import { Label } from "@mui/icons-material";
+import { useAuth } from "../../../context/AuthContext";
 
 interface IReviewFormProps {
     paperId: any;
@@ -10,6 +11,7 @@ interface IReviewFormProps {
 
 function ReviewForm(props: IReviewFormProps) {
     const navigate = useNavigate();
+    const { authState } = useAuth();
     const [rows, setRows] = useState<any[]>([]);
 
     // get reviews
@@ -24,15 +26,16 @@ function ReviewForm(props: IReviewFormProps) {
     const handleViewReviewClick = (data: any) => {
         navigate(`/AuthorRateReview`, { state: { data: data } })
     };
-
+    // console.log(authState.userData)
     return (
         <div style={{ marginBottom: "8px" }}>
             {rows.map((x, index) => (
                 <div key={`review-${index}`} style={{ backgroundColor: "#D9D9D9", marginBottom: "8px", textAlign: "left", padding: "8px" }}>
                     <label>Reviewer: </label><span>{x.reviewername}</span><br />
-                    {x.review === null && (<label>No Review</label>)}
+                    {x.review === null && (<label>No Review <br /></label>)}
                     {x.review !== null && (<label>Rating: {x.paperrating} <br /></label>)}
-                    {x.review !== null && (<span style={{ textDecorationLine: "underline", cursor: "pointer" }} onClick={() => handleViewReviewClick(x)}>View Review</span>)}
+                    {authState.userData.accountType === "AUTHOR" && x.review !== null && (<label>My Rating: {x.reviewrating} <br /></label>)}
+                    {x.review !== null && (<span style={{ textDecorationLine: "underline", cursor: "pointer" }} onClick={() => handleViewReviewClick(x)}>View Review <br /></span>)}
 
                 </div>
             ))}
